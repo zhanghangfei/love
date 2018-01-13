641db1eba733973a8117df46a08a7dca37fff4c6
zhanghangfei <616709127@qq.com>
2018-01-13 09:55:47 +0800

    项目与tp

---
+++ public/艾格尼丝有限公司/public_html/js/index.js
@@ -0,0 +1,3 @@
+jQuery(".banner").slide({mainCell:".pic ul",titCell:".hd li",autoPlay:true});
+jQuery(".pro-list").slide({titCell:".hd ul",mainCell:".pro-content ul",autoPage:true,effect:"left",autoPlay:true,vis:4});
+jQuery(".news_right").slide({});

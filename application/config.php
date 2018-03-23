a90e575a39bf0674e819abe894f2fc6ef85d4c97
zhanghangfei <616709127@qq.com>
2018-03-23 09:50:41 +0800

    添加项目

--- application/config.php
+++ application/config.php
@@ -17,7 +17,7 @@ return [
     // 应用命名空间
     'app_namespace'          => 'app',
     // 应用调试模式
-    'app_debug'              => true,
+    'app_debug'              => false,
     // 应用Trace
     'app_trace'              => false,
     // 应用模式状态
@@ -141,7 +141,10 @@ return [
     ],
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [],
+    'view_replace_str'       => [
+            'root'=>'/',
+            '__ADMIN__' => '/tp5/public/static/admin',
+    ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
     'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
@@ -230,15 +233,6 @@ return [
         'httponly'  => '',
         // 是否使用 setcookie
         'setcookie' => true,
-    ],
-
-    // 视图输出字符串内容替换
-
-    'view_replace_str' => [
-        '__ROOT__' => __ROOT__,
-
-        '__INDEX__'=>__ROOT__ .'/public/static/index',
-
     ],
 
     //分页配置
--- public/艾格尼丝有限公司/public_html/js/index.js
+++
@@ -1,3 +0,0 @@
-jQuery(".banner").slide({mainCell:".pic ul",titCell:".hd li",autoPlay:true});
-jQuery(".pro-list").slide({titCell:".hd ul",mainCell:".pro-content ul",autoPage:true,effect:"left",autoPlay:true,vis:4});
-jQuery(".news_right").slide({});
